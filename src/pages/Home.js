d03ab025616cf0da71fee44ec21f2fbc9879acca
Sankhya Siddesh <siddeshsankhya@gmail.com>
2022-11-08 22:48:28 +0530

    UI updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,52 +1,58 @@
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from "react";
 
 function Home() {
-  const [movie,setMovie]=useState([]);
-  const [inp,setInp]=useState("");
-  const [titles,setTitle]=useState([]);
-  var arr=[]
-  const getMovies=()=>{
-    const url=`https://www.omdbapi.com/?apikey=ead0329f&s=${inp}`;
+  const [movie, setMovie] = useState([]);
+  const [inp, setInp] = useState("");
+  const [titles, setTitle] = useState([]);
+  var arr = [];
+  const getMovies = () => {
+    const url = `https://www.omdbapi.com/?apikey=ead0329f&s=${inp}`;
 
     fetch(url)
-    .then((response) => response.json())
-    .then((data) => setMovie(data.Search));
-    var arr=[]
-    if(movie!=null){
-    movie.map(item=>{arr.push(item.Title);})
-    console.log(arr);
-    setTitle(arr);
+      .then((response) => response.json())
+      .then((data) => setMovie(data.Search));
+    var arr = [];
+    if (movie != null) {
+      movie.map((item) => {
+        arr.push(item.Title);
+      });
+      console.log(arr);
+      setTitle(arr);
     }
-    
-  }
-  
-  const handleChange=(event)=>{
+  };
+
+  const handleChange = (event) => {
     setInp(event.target.value);
     getMovies();
- 
-  }
-
+  };
 
   return (
-  <div className="searchBox">
-    <h1>Search for your fav movie!</h1>
-    <fieldset className="field-container">
-      <form method="get" action={`/${inp}`}>
-      <input type="text" placeholder="Start typing....." className="field" value={inp}  onChange={handleChange} />
-
-      </form>
-    </fieldset>
-    <div className='results'>
-      {titles.length!=0 &&     
-        <>
-          {titles.map(title => (
-            <a href={title}>{title}</a>
-          ))}
-        </>
-      }
+    <div className="searchBox">
+      <h1 id="tit">Search for your fav movie!</h1>
+      <fieldset className="field-container">
+        <form method="get" action={`/${inp}`}>
+          <input
+            type="text"
+            placeholder="Start typing....."
+            className="field"
+            value={inp}
+            onChange={handleChange}
+          />
+        </form>
+      </fieldset>
+      <div className="results">
+        {titles.length != 0 && (
+          <>
+            {titles.map((title) => (
+              <a id="link-a" href={title}>
+                {title}
+              </a>
+            ))}
+          </>
+        )}
+      </div>
+      <h2 id="foo">Your mini-movie library :)</h2>
     </div>
-    <h2 id='foo'>Your mini-movie library :)</h2>
-  </div>
   );
 }
 
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,50 +1,72 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import {useState,useEffect} from 'react'
-function Card(props){
-    return (
-        <div className='child'>
-            <div className='img-box'>
-            <img src={props.img} />
-            </div>
-            <div className='txt'>
-            <h4>{props.title}</h4>
-            <h5>Year: {props.year}</h5>
-            </div>
-        </div>
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+function NewCard(props) {
+  const [watchProvider, setWatchProvider] = useState("");
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${props.imdbID}/watch/providers?api_key=d8482599fceec91ff49e468938c0bb29`
     )
+      .then((response) => response.json())
+      .then((data) => setWatchProvider(data.results.US.link));
+  }, []);
+  console.log(watchProvider);
+  return (
+    <div class="main-container">
+      <div class="poster-container">
+        <a href={watchProvider} target="_blank" rel="noreferrer">
+          <img src={props.img} alt="banner" class="poster" />
+        </a>
+      </div>
+      <div class="ticket-container">
+        <div class="ticket__content">
+          <h4 class="ticket__movie-title">{props.title}</h4>
+          <p class="ticket__year">{props.year}</p>
+          <a href={watchProvider} target="_blank" rel="noreferrer">
+            <button class="ticket__buy-btn">Check out</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 function Search() {
-    const [result, setResult]=useState([])
-    const queryParams = new URLSearchParams(window.location.search);
-    let {search}=useParams();
-    console.log(search);
-    const url=`https://www.omdbapi.com/?apikey=ead0329f&s=${search}`
-    useEffect(() => {
+  const [result, setResult] = useState([]);
+  const queryParams = new URLSearchParams(window.location.search);
+  let { search } = useParams();
+
+  const url = `https://www.omdbapi.com/?apikey=ead0329f&s=${search}`;
+  useEffect(() => {
     fetch(url)
-        .then((response) => response.json())
-        .then((data) =>data.Search!= undefined ? setResult(data.Search) : []);
-        
-    }, []);
+      .then((response) => response.json())
+      .then((data) => (data.Search != undefined ? setResult(data.Search) : []));
+  }, []);
 
-    return (
-        <>
-        <h1 id="tit">Related Movies</h1>
-        <p id='ps'>Search Again? <a href='/'>Click here</a></p>
-        <div className='child-container'>
-           
-            {
-                result.map((item)=>{
-                    return(
-                        <Card img={item.Poster!="N/A" ? item.Poster : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.myhappyenglish.com%2Fx9walos9f%2Fuploads%2F2013%2F05%2Fplaceholder1.png&f=1&nofb=1"} title={item.Title} year={item.Year} />
-                    )
-                })   
-            }
-        </div>
-        <h2 id='foo'>Your mini-movie library :)</h2>
-        </>
-
-    );
+  return (
+    <>
+      <h1 id="tit">Related Movies</h1>
+      <p id="ps">
+        Search Again? <a href="/">Click here</a>
+      </p>
+      <div className="hero-container">
+        {result.map((item) => {
+          return (
+            <NewCard
+              img={
+                item.Poster != "N/A"
+                  ? item.Poster
+                  : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.myhappyenglish.com%2Fx9walos9f%2Fuploads%2F2013%2F05%2Fplaceholder1.png&f=1&nofb=1"
+              }
+              title={item.Title}
+              year={item.Year}
+              imdbID={item.imdbID}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Search;
